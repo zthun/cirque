036b463bd8f6c5a72d0fe8673abc437c3c125183
Anthony Bonta <zthun.coding@gmail.com>
2023-12-15 15:37:35 -0600

    build: update to use vite in library mode

--- packages/cirque/src/index.ts
+++ packages/cirque/src/index.ts
@@ -1,23 +1,9 @@
-/* c8 ignore start */
-
-// Component
-export { ZCircusBy } from './component/circus-by';
-export { ZCircusComponentConstructor, ZCircusComponentModel } from './component/circus-component-model';
-// Driver
-export { IZCircusDriver } from './driver/circus-driver';
-export { IZCircusWaitOptions, ZCircusWaitOptionsBuilder } from './driver/circus-wait-options';
-// Keyboard
-export { IZCircusKey } from './keyboard/circus-key';
-export { ZCircusKeyboardQwerty } from './keyboard/circus-keyboard-qwerty';
-// Performance
-export { IZCircusAct, ZCircusActBuilder } from './performance/circus-act';
-export {
-  isKeyboardAction,
-  isMagicAction,
-  isMouseAction,
-  IZCircusAction,
-  ZCircusActionType,
-  ZCircusMagicFunction,
-  ZCircusMouseButton
-} from './performance/circus-action';
-export { IZCircusSetup } from './performance/circus-setup';
+export * from './component/circus-by';
+export * from './component/circus-component-model';
+export * from './driver/circus-driver';
+export * from './driver/circus-wait-options';
+export * from './keyboard/circus-key';
+export * from './keyboard/circus-keyboard-qwerty';
+export * from './performance/circus-act';
+export * from './performance/circus-action';
+export * from './performance/circus-setup';
