0ffa1bdf448fc61805c67298992cf1e079cea1e1
Anthony Bonta <zthun.coding@gmail.com>
2023-12-15 16:12:08 -0600

    build: convert to vite

--- packages/cirque-du-selenium/src/index.ts
+++ packages/cirque-du-selenium/src/index.ts
@@ -1,7 +1,6 @@
-// Driver
-export { ZCircusDriver } from './driver/circus-driver';
-// Setup
-export { ZCircusSetupBrowser } from './setup/circus-setup-browser';
-export { ZCircusSetupChrome } from './setup/circus-setup-chrome';
-export { ZCircusSetupEdge } from './setup/circus-setup-edge';
-export { ZCircusSetupFirefox } from './setup/circus-setup-firefox';
+export * from './driver/circus-driver';
+export * from './setup/circus-setup-browser';
+export * from './setup/circus-setup-chrome';
+export * from './setup/circus-setup-edge';
+export * from './setup/circus-setup-firefox';
+
---
+++ packages/cirque-du-selenium/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import { externalizeDeps } from 'vite-plugin-externalize-deps';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      formats: ['es', 'cjs'],
+      fileName: 'index'
+    },
+    sourcemap: true
+  },
+  plugins: [externalizeDeps(), dts({ tsconfigPath: 'tsconfig.prod.json' })]
+});
