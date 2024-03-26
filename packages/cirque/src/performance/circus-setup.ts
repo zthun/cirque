cfd131cea4fc130f634a571acdd14a707fcf4b39
Anthony Bonta <zthun.coding@gmail.com>
2024-03-26 17:04:45 -0500

    feat: circus setup type now defaults to IZCircusDriver

--- packages/cirque/src/performance/circus-setup.ts
+++ packages/cirque/src/performance/circus-setup.ts
@@ -1,7 +1,13 @@
+import { IZCircusDriver } from '../driver/circus-driver';
+
 /**
  * Represents a step to setup the circus.
+ *
+ * @param T -
+ *        The type of data the setup will return.  Defaults
+ *        to a circus driver.
  */
-export interface IZCircusSetup<T> {
+export interface IZCircusSetup<T = IZCircusDriver> {
   /**
    * Releases and cleans up any items used by this setup.
    */
