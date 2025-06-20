6fb90ee2214ccbefc6fa45b9913df707cf964cdd
Anthony Bonta <zthun.coding@gmail.com>
2025-06-19 22:34:17 -0500

    build: lint with common structure

--- .config/janitorrc.mjs
+++ .config/janitorrc.mjs
@@ -4,25 +4,14 @@ import {
 } from "@zthun/janitor-options";
 
 const lint = new ZJanitorOptionsLintBuilder()
-  .esFile("*.{js,cjs,mjs,ts,mts,tsx}")
-  .esFile("packages/**/src/**/*.{js,cjs,mjs,ts,mts,tsx}")
-  .htmlFile("packages/**/*.html")
-  .markdownFile("*.md")
-  .markdownFile("packages/**/*.md")
-  .jsonFile("*.json")
-  .jsonFile("packages/**/*.json")
-  .yamlFile(".circleci/config.yml")
+  .commonEsFiles()
+  .commonHtmlFiles()
+  .commonMarkdownFiles()
+  .commonJsonFiles()
+  .commonYamlFiles()
   .generateSpellingFiles()
   .generatePrettyFiles()
-  .excludeAll("**/CHANGELOG.md")
-  .excludeAll("packages/**/dist/**")
-  .excludeAll("packages/**/docs/**")
-  .excludeAll("node_modules/**")
-  .excludeAll("packages/**/node_modules/**")
-  .excludeAll(".yarnrc.yml")
-  .excludeAll("yarn.lock")
-  .excludeAll("lerna.json")
-  .excludeAll(".config/cspell.json")
+  .commonExcludes()
   .build();
 
 export default new ZJanitorOptionsBuilder().lint(lint).build();
